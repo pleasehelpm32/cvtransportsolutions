72108e10cefc5ecb86ebce894294d77b998e8df7
Josh <josh.si@outlook.com>
2024-08-15 16:42:43 -0400

    adding functions to netlify

---
+++ functions/functions/send-email.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const { name, email, phone, inquiry } = JSON.parse(event.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: "contact@cvtransportsolutions.com",
+    subject: "New Contact Form Submission",
+    text: `
+         Name: ${name}
+         Email: ${email}
+         Phone: ${phone || "Not provided"}
+         Inquiry: ${inquiry}
+       `,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error sending email" }),
+    };
+  }
+};
